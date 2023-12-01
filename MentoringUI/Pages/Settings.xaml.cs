d90a8ac5bc94f7a650e91ed6456b8ec0e3dde9ed
Zechmeister Benjamin <benjamin.zechmeister@htlwy.at>
2023-12-01 22:23:28 +0100

    Implemented: on every tick open ChangeColor

--- MentoringUI/Pages/Settings.xaml.cs
+++ MentoringUI/Pages/Settings.xaml.cs
@@ -32,6 +32,7 @@ namespace MentoringUI
 
             Index = index;
             timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += ChangeColor;
         }
             Brush? dark = (Brush?)new BrushConverter().ConvertFromString("#505050");
             Brush? light = (Brush?)new BrushConverter().ConvertFromString("#FFFFFF");
