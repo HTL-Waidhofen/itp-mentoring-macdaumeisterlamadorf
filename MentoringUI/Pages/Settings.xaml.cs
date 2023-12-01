0f941b469c1e83d7b463e5691aacd9aae729340a
Zechmeister Benjamin <benjamin.zechmeister@htlwy.at>
2023-12-01 15:55:10 +0100

    Implemented SetColor for the defaultColor

--- MentoringUI/Pages/Settings.xaml.cs
+++ MentoringUI/Pages/Settings.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using i18n;
@@ -40,24 +41,30 @@ namespace MentoringUI
                     switch (int.Parse(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize", "AppsUseLightTheme", 1).ToString()))
                     {
                         case 0:
+                            SetColor(light, dark);
+                    return_btn_img.Source = new BitmapImage(new Uri("/Images/return_button_dark.png", UriKind.Relative));
                         break;
                         case 1:
+                            SetColor(dark, light);
+                    return_btn_img.Source = new BitmapImage(new Uri("/Images/return_button_white.png", UriKind.Relative));
                         break;
                     }
                     break;
                 case 1:
-                    ((MainWindow)Application.Current.MainWindow).Background = dark;
-                    ((MainWindow)Application.Current.MainWindow).Foreground = light;
+                    SetColor(light, dark);
                     return_btn_img.Source = new BitmapImage(new Uri("/Images/return_button_white.png", UriKind.Relative));
                     break;
                 case 2:
-                    ((MainWindow)Application.Current.MainWindow).Background = light;
-                    ((MainWindow)Application.Current.MainWindow).Foreground = dark;
+                    SetColor(dark, light);
                     return_btn_img.Source = new BitmapImage(new Uri("/Images/return_button_black.png", UriKind.Relative));
                     break;
             }
         }
-
+        private void SetColor(Brush foreground, Brush background)
+        {
+            ((MainWindow)Application.Current.MainWindow).Background = background;
+            ((MainWindow)Application.Current.MainWindow).Foreground = foreground;
+        }
         private void language_cbx_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             
