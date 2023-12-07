6e7c12c5bd37a0fe4d64621faeb1d7df55e9d89e
Guntendorfer Konrad <konrad.guntendorfer@htlwy.at>
2023-12-07 09:45:24 +0100

    made a click event in setting button

--- MentoringUI/Pages/UserManagement_Mentors.xaml.cs
+++ MentoringUI/Pages/UserManagement_Mentors.xaml.cs
@@ -24,5 +24,10 @@ namespace MentoringUI
         {
             InitializeComponent();
         }
+
+        private void settings_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
