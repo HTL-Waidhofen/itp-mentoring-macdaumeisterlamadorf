52d4219eb546121e4b38806852d5896b08ba428a
Guntendorfer Konrad <konrad.guntendorfer@htlwy.at>
2023-12-07 08:08:42 +0100

    added a click element to the setting button

--- MentoringUI/Pages/Student.xaml.cs
+++ MentoringUI/Pages/Student.xaml.cs
@@ -24,5 +24,10 @@ namespace MentoringUI
         {
             InitializeComponent();
         }
+
+        private void setting_btn_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
     }
 }
