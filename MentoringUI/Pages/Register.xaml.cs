0e88cce018eb51e30a8f906d12526ee11c6583b7
ZechmeisterBenjamin <144880216+ZechmeisterBenjamin@users.noreply.github.com>
2024-01-24 16:49:11 +0100

    Fixed Register CodeBehind to check if everything is filled out correctly

--- MentoringUI/Pages/Register.xaml.cs
+++ MentoringUI/Pages/Register.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -233,23 +234,29 @@ namespace MentoringUI
 
         private void register_btn_Click(object sender, RoutedEventArgs e)
         {
-            
-            if(!string.IsNullOrEmpty(firstname_tbx.Text) && string.IsNullOrEmpty(name_tbx.Text) && string.IsNullOrEmpty(email_tbx.Text) && string.IsNullOrEmpty(department_cbx.SelectedValue) && string.IsNullOrEmpty(class_cbx.Text) && string.IsNullOrEmpty(pwd_pbx.Password) && string.IsNullOrEmpty(confirmpwd_pbx.Password))
+            if(!string.IsNullOrEmpty(firstname_tbx.Text) && !string.IsNullOrEmpty(name_tbx.Text) && !string.IsNullOrEmpty(email_tbx.Text) && department_cbx.SelectedIndex != -1 && class_cbx.SelectedIndex != -1 && !string.IsNullOrEmpty(pwd_pbx.Password) && !string.IsNullOrEmpty(confirmpwd_pbx.Password))
             {
-                string pattern = "^[\\w-.]+@([\\w-]+.)+[\\w-]{2,4}$\r\n(?:[a-z0-9!#$%&'+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'+/=?^_`{|}~-]+)|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])).){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])";
-                bool isEmailcorrect = Regex.IsMatch(email_tbx.Text, pattern);
-
-                if (isEmailcorrect == true && pwd_pbx.Password == confirmpwd_pbx.Password)
+                if (IsValidEmail(email_tbx.Text) && pwd_pbx.Password == confirmpwd_pbx.Password)
                 {
                     MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
                     mainWindow.Content = new Email_Verification();
                 }
             }
+        }
+        private bool IsValidEmail(string email)
+        {
+            var valid = true;
 
-            
+            try
+            {
+                var emailAddress = new MailAddress(email);
+            }
+            catch
+            {
+                valid = false;
+            }
 
-            
-            
+            return valid;
         }
     }
 }
