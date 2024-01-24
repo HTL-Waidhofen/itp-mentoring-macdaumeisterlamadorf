eba9cce4d93a844078a9a2e0fcc06ff1191e2ef2
ZechmeisterBenjamin <144880216+ZechmeisterBenjamin@users.noreply.github.com>
2024-01-24 23:49:35 +0100

    Making mentor able to chose what courses he wants to teach

--- MentoringUI/Pages/Mentors.xaml.cs
+++ MentoringUI/Pages/Mentors.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLiteDataAccess;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -21,27 +22,38 @@ namespace MentoringUI
     /// </summary>
     public partial class Mentors : Page
     {
-        private ObservableCollection<string> allMentors;
+        string connectionString = @"Data Source=..\..\..\..\Database\itpmentoring.db;Version=3;";
+
         SQLiteDataAccess.User user;
         public Mentors(SQLiteDataAccess.User user)
         {
             InitializeComponent();
 
-            filterTextBox.TextChanged += FilterTextBox_TextChanged;
             this.user = user;
+            List<Course> courses = SQLiteDataAccess.SqliteDataAccess.LoadCourses(connectionString);
+            courses_lbx.Items.Clear();
+            for (int i = 0; i < courses.Count; i++) courses_lbx.Items.Add(courses[i]);
         }
-        private void FilterTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
-        {
-            string filterText = filterTextBox.Text.ToLower();
-
-            var filteredMentors = allMentors.Where(mentor => mentor.ToLower().Contains(filterText)).ToList();
-
-            mentorslbx.ItemsSource = filteredMentors;
-        }
-
         private void settings_btn_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.MainWindow.Content = new Settings(4, user);
+        }
+
+        private void saveCourses_btn_Click(object sender, RoutedEventArgs e)
+        {
+            string courses = "";
+            for(int i = 0; i < courses_lbx.SelectedItems.Count; i++)
+            {
+                courses += courses_lbx.SelectedItems[i].ToString() + ",";
+            }
+            courses = courses.Substring(0, courses.Length - 1);
+            List<Mentor> mentors = SqliteDataAccess.LoadMentors(connectionString);
+            Mentor mentor = null;
+            for (int i = 0; i < mentors.Count; i++)
+            {
+                if (mentors[i].MentorID == user.MentorID_FK) mentor = mentors[i];
+            }
+            SqliteDataAccess.UpdateMentor(connectionString, new Mentor(mentor.MentorID, courses));
         }
     }
 }
