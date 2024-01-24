ef61c2486740e62682812d444214fa23d820f791
Lamerana Emanuel <emanuel.lamerana@htlwy.at>
2024-01-24 20:03:11 +0100

    Added baseline for an automatic swipe feature and a list of Picture File Paths for the Collage

--- MentoringUI/Pages/Startpage.xaml.cs
+++ MentoringUI/Pages/Startpage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MentoringUI
 {
@@ -20,9 +22,25 @@ namespace MentoringUI
     /// </summary>
     public partial class Startpage : Page
     {
+        private DispatcherTimer autoSwipeTimer;
+        private int autoSwipeInterval = 3000;
+
+        string[] pictures =
+        {
+            "Images/HTL-Logo.png",
+            "Images/Beachtime.png",
+            "Images/Strand.jpg"
+        };
+
         public Startpage()
         {
             InitializeComponent();
-        }      
+
+            autoSwipeTimer = new DispatcherTimer();
+            autoSwipeTimer.Tick += AutoSwipeTimer_Tick;
+            autoSwipeTimer.Interval = TimeSpan.FromMilliseconds(autoSwipeInterval);
+
+            autoSwipeTimer.Start();
+        }
     }
 }
