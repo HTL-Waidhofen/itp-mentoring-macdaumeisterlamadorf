b2a41b11c4be30d13ca458e50b9733c15a20a097
ZechmeisterBenjamin <144880216+ZechmeisterBenjamin@users.noreply.github.com>
2024-01-25 00:31:55 +0100

    Deleting and editing courses

--- MentoringUI/Pages/CourseManagment.xaml.cs
+++ MentoringUI/Pages/CourseManagment.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLiteDataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,21 +22,57 @@ namespace MentoringUI
     public partial class CourseManagment : Page
     {
         SQLiteDataAccess.User user;
+        string connectionString = @"Data Source=..\..\..\..\Database\itpmentoring.db;Version=3;";
         public CourseManagment(SQLiteDataAccess.User user)
         {
             InitializeComponent();
             this.user = user;
+            List<Course> courses = SQLiteDataAccess.SqliteDataAccess.LoadCourses(connectionString);
+            Course_lbx.Items.Clear();
+            for (int i = 0; i < courses.Count; i++) Course_lbx.Items.Add(courses[i].ToString());
         }
         private void settings_btn_Click(object sender, RoutedEventArgs e)
         {
             MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
             mainWindow.Content = new Settings(6, user);
         }
-        private void delete(object sender, RoutedEventArgs e)
+        public void edit_btn_Click(object sender, RoutedEventArgs e)
         {
-            if (Course_lbx.SelectedItems.Count != 0)
+
+            if (!string.IsNullOrEmpty(Course_lbx.SelectedItem.ToString()))
             {
-                MessageBoxResult result = MessageBox.Show("Are you sure you want to delete these Mentors?", "Warning", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                var dialog = new InputDialog(Course_lbx.SelectedItem.ToString());
+
+                bool? result = dialog.ShowDialog();
+
+                if (result == true)
+                {
+                    MessageBoxResult mResult = MessageBox.Show("Sind Sie sicher, dass Sie diesen Kurs bearbeiten wollen?", "Warning", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                    if (mResult == MessageBoxResult.Yes)
+                    {
+                        List<Course> allCourses = SqliteDataAccess.LoadCourses(connectionString);
+                        int courseID = -1;
+                        for(int i = 0; i < allCourses.Count; i++)
+                            if(allCourses[i].Coursename == Course_lbx.SelectedItem.ToString()) courseID = allCourses[i].CourseID;
+                        Course_lbx.SelectedItem = dialog.InputText;
+                        SqliteDataAccess.UpdateCourse(connectionString, new Course(courseID, dialog.InputText));
+                        List<Course> courses = SQLiteDataAccess.SqliteDataAccess.LoadCourses(connectionString);
+                        Course_lbx.Items.Clear();
+                        for (int i = 0; i < courses.Count; i++) Course_lbx.Items.Add(courses[i].ToString());
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Bitte wählen Sie einen Kurs aus, um ihn zu bearbeiten.");
+            }
+        }
+
+        public void delete_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (Course_lbx.SelectedItems.Count == 1)
+            {
+                MessageBoxResult result = MessageBox.Show("Sind Sie sicher, dass Sie diesen Kurs löschen wollen?", "Warning", MessageBoxButton.YesNo, MessageBoxImage.Warning);
                 if (result == MessageBoxResult.Yes)
                 {
                     DeleteSelectedItems();
@@ -43,25 +80,22 @@ namespace MentoringUI
             }
             else
             {
-                MessageBox.Show("Select Mentors to delete them", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show("Wählen Sie einen Kurs aus, um diesen zu löschen", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
             }
         }
         private void DeleteSelectedItems()
         {
             var selectedItems = new List<object>(Course_lbx.SelectedItems.Cast<object>());
+
             foreach (var item in selectedItems)
             {
+                List<Course> allCourses = SqliteDataAccess.LoadCourses(connectionString);
                 Course_lbx.Items.Remove(item);
+                int courseID = -1;
+                for (int i = 0; i < allCourses.Count; i++)
+                    if (allCourses[i].Coursename == item.ToString()) courseID = allCourses[i].CourseID;
+                SqliteDataAccess.DeleteCourse(connectionString, courseID);
             }
-        }
-        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            double newHeight = e.NewSize.Height - 180;
-            Course_lbx.Height = newHeight;
-        }
-        private void ListBoxItem_Unchecked(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
